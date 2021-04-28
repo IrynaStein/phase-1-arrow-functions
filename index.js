fa0cc7c329af721d38d0041b1e8048b6a44e4d94
Iryna Stein <isophiestein@gmail.com>
2021-04-27 18:50:52 -0600

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+// function divide (x=2000,y=100){
+//     return x/y
+// };
+
+const x = 2000;
+const y = 100;
+const divide = function() {
+    return x/y;
+};
+
+const square = x => x * 2;
+
+const add = (x,y) => x + y;
